74c6ff323c55f1375c8bcbb0953147f6509f1b96
Ale <133436298+afuenteshinojosa@users.noreply.github.com>
2023-07-12 07:53:11 -0400

    proyecto_recetas

--- app_recetas/controladores/controlador_recetas.py
+++ app_recetas/controladores/controlador_recetas.py
@@ -1,75 +1,73 @@
-from flask import render_template, request, session, redirect
+from flask import render_template, redirect, request, session
 from app_recetas.modelos.modelo_recetas import Receta
 from app_recetas import app
 
-@app.route('/recetas', methods = ['GET'])
+@app.route ('/recetas', methods = ['GET'])
 def desplegar_recetas():
     if "id_usuario" not in session:
-        return redirect ('/')
+        return redirect('/')
     else:
-        lista_recetas = Receta.obtener_todas_con_usuario()
-        #enviar la lista de recetas hacia el template
-        return render_template('recetas.html', lista_recetas = lista_recetas)
+        lista_recetas = Receta.obtener_todas_con_usuarios()
+        return render_template ('/recetas.html', lista_recetas = lista_recetas)
+    
 
-@app.route('/formulario/receta', methods = ['GET'])
+@app.route ('/formulario/receta', methods =['GET'])
 def desplegar_formulario_receta():
     if "id_usuario" not in session:
-        return redirect ('/')
-    else:    
-        return render_template('formulario_receta.html')
+        return redirect('/')
+    else:
+        return render_template ('formulario_receta.html')
 
-@app.route('/crear/receta', methods = ['POST'])
+@app.route ('/crear/receta', methods =['POST'])
 def nueva_receta():
-    data={
+    data = {
         **request.form,
         "id_usuario" : session['id_usuario']
     }
-    if Receta.validar_formulario_recetas( data ) == False:
+    if Receta.validar_formulario_recetas ( data) == False:
         return redirect('/formulario/receta')
     else:
-        id_receta = Receta.crear_uno( data )
+        id_receta = Receta.crear_uno ( data )
         return redirect('/recetas')
     
-@app.route('/eliminar/receta/<int:id>', methods = ['POST'])
-def eliminar_receta(id):
+@app.route ('/eliminar/receta/<int:id>', methods = ['POST'])
+def eliminar_receta (id):
     data = {
         "id" : id
     }
-    Receta.elimina_uno( data )
+    Receta.elimina_uno (data)
     return redirect ('/recetas')
 
-@app.route('/receta/<int:id>', methods = ['GET'])
-def desplegar_receta( id ):
+@app.route('/receta/<int:id>', methods =['GET'])
+def desplegar_receta(id):
     if "id_usuario" not in session:
-        return redirect ('/')
-    else:    
-        data = {
+        return redirect('/')
+    else:
+        data ={
             "id" : id
         }
         receta = Receta.obtener_uno_con_usuario(data)
-        return render_template( 'receta.html', receta = receta)
+        return render_template('receta.html', receta = receta)
 
-@app.route('/formulario/editar/receta/<int:id>', methods = ['GET'])
+@app.route('/formulario/editar/receta/<int:id>', methods =['GET'])
 def desplegar_editar_receta(id):
     if "id_usuario" not in session:
-        return redirect ('/')
-    else:    
-        data = {
+        return redirect('/')
+    else:
+        data ={
             "id" : id
         }
-        receta = Receta.obtener_uno( data )
-        return render_template( 'editar_receta.html', receta = receta )
+        receta = Receta.obtener_uno(data)
+        return render_template('editar_receta.html', receta = receta)
 
-@app.route('/editar/receta/<int:id>', methods = ['POST'])
+@app.route('/editar/receta/<int:id>', methods =['POST'])
 def editar_receta(id):
-    if Receta.validar_formulario_recetas( request.form ) == False:
-        return redirect( f'/formulario/editar/receta/{id}' )
+    if Receta.validar_formulario_recetas (request.form) == False:
+        return redirect ( f'/formulario/editar/receta/{id}' )
     else:
         data = {
             **request.form,
-            "id" : id,
-            
+            "id" : id
         }
-        Receta.editar_uno(data)
-        return redirect( '/recetas' )
-    
+        Receta.editar_uno (data)
+        return redirect ('/recetas')
